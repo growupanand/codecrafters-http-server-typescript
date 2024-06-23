5002b48072bb47f74630518514578e02a89dc49b
Utkarsh Anand <growupanand@gmail.com>
2024-06-23 22:35:03 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -11,10 +11,12 @@ const directoryArgValue = directoryArgIndex !== -1 ? processArgs[directoryArgInd
 const server = net.createServer((socket) => {
 
     socket.on('data', (data)=>{
-        const request = data.toString();
-        const path = request.split(' ')[1];
+        const requestString = data.toString();
+        const requestLines = requestString.split('\r\n');
+        const [method, path, _] = requestLines[0].split(' ');
+        const requestBody = requestLines.pop();
 
-        const headersStringArray = request.split('\r\n').slice(1, -2);
+        const headersStringArray = requestLines.slice(1);
         const headersKeyValueArray: [string, string][] = headersStringArray.map(headerString => {
             const [key, _ ] = headerString.split(': ');
             const value = headerString.split(': ')[1];
@@ -23,39 +25,51 @@ const server = net.createServer((socket) => {
         const headersMap = new Map(headersKeyValueArray);
 
         
-        let response;
+        let response = 'HTTP/1.1 404 Not Found\r\n\r\n';
         switch (true) {
             case path === "/":
                 response = 'HTTP/1.1 200 OK\r\n\r\n';
                 break;
 
+            // Example: "GET /echo/abc HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n"
             case path.startsWith("/echo/"):
                 const echoPathQuery = path.split("/echo/")[1];
                 response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoPathQuery.length}\r\n\r\n${echoPathQuery}`;
                 break;
 
             case path === '/user-agent':
+                console.log({headersMap, path});
                 const userAgent = headersMap.get('User-Agent');
                 if (!userAgent) {
                     response = 'HTTP/1.1 400 Bad Request\r\n\r\n';
                     break;
                 }
+
                 response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
                 break;
 
             case path.startsWith("/files/"):
                 const fileName = path.split("/files/")[1];
                 const filePath = directoryArgValue ? `${directoryArgValue}/${fileName}` : fileName;
-                try {
-                    const fileContent = fs.readFileSync(filePath);
-                    response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
-                } catch (error) {
-                    response = 'HTTP/1.1 404 Not Found\r\n\r\n';
+
+                if (method === "GET") {
+                    try {
+                        const fileContent = fs.readFileSync(filePath);
+                        response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
+                    } catch (error) {}
                 }
-                break;
                 
+                if (method === "POST" && requestBody !== undefined) {
+                    try {
+                        // Create new file with content of request body
+                        fs.writeFileSync(filePath, requestBody);
+                        response = 'HTTP/1.1 201 Created\r\n\r\n';
+                    } catch (error) {}
+                }
+
+                break;
+
             default:
-                response = 'HTTP/1.1 404 Not Found\r\n\r\n';
                 break;
         }
 
